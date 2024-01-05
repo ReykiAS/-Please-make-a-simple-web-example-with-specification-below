2f360ceb6dfdbaff8d9dd5009e3e4a508f9c8daf
ReykiAs <foxko80@gmail.com>
2024-01-05 14:52:06 +0700

    Timedoor Test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\books::factory(3000)->create();
-        //  \App\Models\author::factory(1000)->create();
-        //  \App\Models\category::factory(3000)->create();
-        //  \App\Models\ratings::factory(25000)->create();
+          \App\Models\books::factory(100000)->create();
+          \App\Models\author::factory(1000)->create();
+          \App\Models\category::factory(3000)->create();
+         \App\Models\ratings::factory(500000)->create();
        /*  $this->call([
             BooksSeeder::class,
         ]); */
